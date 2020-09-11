6970fb34961076149230bafca426b61640c26ad2
it-long <1962085916@qq.com>
2020-09-11 10:55:34 +0800

    对HelloWorld项目的第一次操作

---
+++ HelloWorld/src/com/qfedu/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.qfedu;
+
+/**
+ * projectName: HelloWorld
+ *
+ * @author: IT-DREG
+ * time: 2020/9/11 10:52
+ * description:
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
