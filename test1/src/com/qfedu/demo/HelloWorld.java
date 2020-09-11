3411cc759e9e316882f95d01e6859556a2c9df65
it-long <1962085916@qq.com>
2020-09-11 13:57:34 +0800

    test1项目提交

---
+++ test1/src/com/qfedu/demo/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.qfedu.demo;
+
+/**
+ * projectName: test1
+ *
+ * @author: IT-DREG
+ * time: 2020/9/11 13:52
+ * description:
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
